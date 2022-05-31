04209969d04f035b6d2aa8fae89adb21c196d23c
Lit Phansiri <lphansiri@gmail.com>
2022-05-31 17:17:24 +0900

    complete working page

---
+++ src/components/ProjectItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-card flat class="text-center q-mx-md">
+    <q-img src="@/assets/p1-uw-nrotc.png" />
+    <q-card-section>
+      <q-separator />
+      <div class="text-h6">
+        <a href="http://nrotc.washington.edu/" target="_blank">UW NROTC</a>
+      </div>
+      <div class="text-subtitle2">
+        Redesigned website to allow easy update while having a professional
+        website. The project was to update their antiquated drupal website to a
+        modern design where any user can quickly update without truely
+        understand programming.
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: Object,
+    },
+  },
+};
+</script>

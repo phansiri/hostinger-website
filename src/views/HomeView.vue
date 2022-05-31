324596ff2996e51da672c63e1c859a5089fade55
Lit Phansiri <lphansiri@gmail.com>
2022-05-31 20:50:31 +0900

    minor update - favicon and page title on each page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,11 +10,29 @@
 import HeroSection from "@/components/HeroSection.vue";
 import MainDesc from "@/components/MainDesc.vue";
 
+import { useMeta } from "quasar";
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
   components: {
     HeroSection,
     MainDesc,
+  },
+  setup() {
+    const title = ref("Lit | Home");
+    useMeta(() => {
+      return {
+        title: title.value,
+      };
+    });
+    function setAnotherTitle() {
+      title.value = "Another title";
+    }
+
+    return {
+      setAnotherTitle,
+    };
   },
 };
 </script>

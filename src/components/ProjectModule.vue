af9be28df4c306483a21fe3803b9538cbfa266c8
Lit Phansiri <lphansiri@gmail.com>
2022-05-31 10:39:47 +0900

    initial layout with template cards

---
+++ src/components/ProjectModule.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="full-width row wrap justify-center">
+    <div class="col-12 col-md">
+      <q-intersection transition="scale" class="example-item">
+        <q-card class="q-ma-sm">
+          <img src="@/assets/boy-avatar.png" />
+
+          <q-card-section>
+            <div class="text-h6">Card #</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+        </q-card>
+      </q-intersection>
+    </div>
+    <div class="col-12 col-md">
+      <q-intersection transition="scale" class="example-item">
+        <q-card class="q-ma-sm">
+          <img src="@/assets/boy-avatar.png" />
+
+          <q-card-section>
+            <div class="text-h6">Card #</div>
+            <div class="text-subtitle2">by John Doe</div>
+          </q-card-section>
+        </q-card>
+      </q-intersection>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // setup() {
+  //   const projects = [
+  //     {
+  //       img: "/img/boy-avatar.png",
+  //       title: "card",
+  //       subtitle: "by Lit",
+  //     },
+  //     {
+  //       img: "/img/boy-avatar.png",
+  //       title: "card",
+  //       subtitle: "by Lit",
+  //     },
+  //     {
+  //       img: "/img/boy-avatar.png",
+  //       title: "card",
+  //       subtitle: "by Lit",
+  //     },
+  //     {
+  //       img: "/img/boy-avatar.png",
+  //       title: "card",
+  //       subtitle: "by Lit",
+  //     },
+  //   ];
+  //   return {
+  //     projects,
+  //   };
+  // },
+};
+</script>
